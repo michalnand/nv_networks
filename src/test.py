38acfa6f74dfd21b2359cef2715255e56a4577f9
michalnand <michal.nand@gmail.com>
2019-02-23 18:51:34 +0100

    common

--- src/test.py
+++ src/test.py
@@ -1,7 +1,56 @@
 import neuron
 
-time_constant = 0.0005
+nv_time_constant = 0.002
+nu_time_constant = 0.001
 
+
+nv_0 = neuron.NVNeuron(nv_time_constant)
+nv_1 = neuron.NVNeuron(nv_time_constant)
+
+nu_0 = neuron.NUNeuron(nu_time_constant)
+nu_1 = neuron.NUNeuron(nu_time_constant)
+
+
+log_file = open("log.txt","w")
+
+for i in range(0, 100):
+    time_ms = round(i*nv_0.get_dt()*1000.0, 1)
+
+    nv_0.process(nv_1.get())
+    nv_1.process(nv_0.get())
+
+    nu_0.process(nv_0.get())
+    nu_1.process(nv_1.get())
+
+
+    if nv_0.get() > nv_1.get():
+        master = 1
+    else:
+        master = -1
+
+    if nu_0.get() > nu_1.get():
+        slave = 1
+    else:
+        slave = -1
+
+    result_str = str(time_ms) + " "
+    result_str+= str(nv_0.get()) + " "
+    result_str+= str(nv_1.get()) + " "
+    result_str+= str(nu_0.get()) + " "
+    result_str+= str(nu_1.get()) + " "
+    result_str+= str(round(nv_0.get_rc_output(), 4)) + " "
+    result_str+= str(round(nv_1.get_rc_output(), 4)) + " "
+    result_str+= str(round(nu_0.get_rc_output(), 4)) + " "
+    result_str+= str(round(nu_1.get_rc_output(), 4)) + "\n"
+
+
+    #print(time_ms, master, slave)
+    print(result_str, end = "")
+    log_file.write(result_str)
+
+log_file.close()
+
+'''
 n0 = neuron.NVNeuron(time_constant)
 n1 = neuron.NVNeuron(time_constant)
 n2 = neuron.NVNeuron(time_constant)
@@ -43,33 +92,4 @@ for i in range(0, 1000):
     time_ms = round(i*n0.get_dt()*1000.0, 1)
     print(time_ms, n0.get(), n1.get(), n2.get(), n3.get(), n4.get(), n5.get(), n6.get(), n7.get())
 
-'''
-time_constant = 0.1
-neurons_count = 6
-
-
-net = []
-for i in range(0, neurons_count):
-    net.append(neuron.NVNeuron(time_constant))
-
-
-
-for i in range(0, 100):
-
-    input = 0.0
-    if i > 10:
-        input = 1.0
-    if i > 50:
-        input = 0.0
-
-
-    for j in range(0, neurons_count):
-        net[j].process(input)
-        input = net[j].get()
-
-    print(i, end = " : ")
-    for j in range(0, neurons_count):
-        print(net[j].get(), end = " ")
-
-    print()
 '''
